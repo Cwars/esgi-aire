ed957dd9fa5b3c83d0e1c5e1bb7722be55401966
Alexandre Ting <alexandre.ting@ymail.com>
2017-07-12 11:37:46 +0200

    Test pour le routing ( pour la mise en ligne )

--- core/Routing.class.php
+++ core/Routing.class.php
@@ -23,8 +23,18 @@ class Routing {
 
     public function setUri ($uri) {
         $uri = preg_replace("/".PATH_RELATIVE_PATTERN."/i", "", $uri, 1);
+        echo PATH_RELATIVE_PATTERN;
+                echo "<br>";
+        var_dump($uri);
+        echo "<br>";
+        var_dump($_SERVER["REQUEST_URI"]);
+        echo "<br>";
         $this->uri = trim($uri, "/");
         $this->uriExploded = explode("/", $this->uri);
+        var_dump($this->uri);
+        echo "<br>";
+        var_dump($this->uriExploded);
+        echo "<br>";
     }
 
     public function setDiff () {
@@ -39,6 +49,7 @@ class Routing {
         if($this->side == "front"){
             $this->controller = (empty($this->uriExploded[0])) ? "index" : $this->uriExploded[0];
             $this->controllerName = $this->controller."Controller";
+            var_dump(  $this->controllerName);
         }else{
             $this->controller = (empty($this->uriExploded[0])) ? "index" : $this->uriExploded[0].$this->uriExploded[1];
             $this->controllerName = $this->controller."Controller";
@@ -51,11 +62,13 @@ class Routing {
         if ($this->side === "front") {
             $this->action = (empty($this->uriExploded[0])) ? "index" : $this->uriExploded[0];
             $this->actionName = $this->action . "Action";
+            var_dump($this->actionName);
             unset($this->uriExploded[0]);
         }
         elseif ($this->side === "back" && !empty($this->uriExploded[2])) {
             $this->action = (empty($this->uriExploded[1])) ? "index" : $this->uriExploded[1] . $this->uriExploded[2];
             $this->actionName = $this->action . "Action";
+            var_dump($this->actionName);
             unset($this->uriExploded[1]);
             unset($this->uriExploded[2]);
         }
@@ -77,6 +90,7 @@ class Routing {
 
         if (!file_exists($pathController)) {
             Helpers::log("Le contrôleur ".$this->controllerName." n'existe pas.");
+            echo "haha";
             return false;
         }
 
@@ -84,15 +98,18 @@ class Routing {
 
         if (!class_exists($this->controllerName)) {
             Helpers::log("La classe ".$this->controllerName." n'existe pas.");
+            echo "hihi";
             return false;
         }
 
         if (!method_exists($this->controllerName, $this->actionName)) {
             Helpers::log("La méthode ".$this->actionName." n'existe pas dans la classe ".$this->controllerName.".");
+            echo "hoho";
             return false;
         }
 
         return true;
+        echo "haha";
     }
 
     public function runRoute() {
@@ -107,11 +124,8 @@ class Routing {
 
     public function page404() {
 
-        $directory = "projetannuelaire";
-        $url = "http://".$_SERVER['HTTP_HOST']."/".$directory;
-
         die("
-            <link rel=\"stylesheet\" href=\"$url/assets/back/css/style.css\"> 
+            <link rel=\"stylesheet\" href=\"".PATH_RELATIVE."./assets/back/css/style.css\"> 
             <div class=\"bg-notf\">
              <div class=\"not-found\">
                 <h1>Erreur</h1>
