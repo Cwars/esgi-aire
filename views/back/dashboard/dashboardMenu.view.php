6da9f230930884d087215258e006b20b217d202e
IlanZ93 <ilan.i.zaoui@gmail.com>
2017-07-25 18:33:50 +0200

    up

--- views/back/dashboard/dashboardMenu.view.php
+++ views/back/dashboard/dashboardMenu.view.php
@@ -1,44 +1,44 @@
-<div class="content-wrapper">
-    <style type="text/css">
-        .graph-container1, .graph-container3{
-            width: 50%;
-            height: auto;
-            float: left;
-        }
-        .graph-container2{
-            width: 100%;
-            height: auto;
-            float: right;
-        }
+<style type="text/css">
+    .graph-container1, .graph-container3{
+        width: 48%;
+        height: auto;
+        float: left;
+        display: inline-block;
+    }
+    .graph-container2{
+        width: 100%;
+        height: auto;
+        float: right;
 
-        h1,h2{
-            text-align: center;
-        }
-        #mygraph3{
-            margin-top: 150px;"
-        }
-    </style>
+    }
+    .content-wrapper {
+        margin: 0 150px!important;
+    }
+    @media (max-width: 12) {
 
-    <h1>Dashboard</h1><br/>
-    <div id="loader"></div>
+    }
+    h1,h2{
+        text-align: center;
+    }
+</style>
 
-    <div class="graph-container2">
-        <div id="dash2">
-            <canvas id="mygraph2"></canvas>
-        </div>
+<h1>Dashboard</h1><br/>
+<div id="loader"></div>
+
+<div class="graph-container2">
+    <div id="dash2">
+        <canvas id="mygraph2"></canvas>
     </div>
+</div>
 
-    <div class="graph-container1">
-        <div id="dash1">
-            <canvas id="mygraph1"></canvas>
-        </div>
+<div class="graph-container1">
+    <div id="dash1">
+        <canvas id="mygraph1"></canvas>
     </div>
+</div>
 
-    <div class="graph-container3">
-        <div id="dash3">
-            <canvas id="mygraph3"></canvas>
-        </div>
+<div class="graph-container3">
+    <div id="dash3">
+        <canvas id="mygraph3"></canvas>
     </div>
-
-</div> <!-- .content-wrapper -->
-<br>
+</div>
